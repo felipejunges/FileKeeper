dc5b6ec1c68b08c2e4b5415f502bfca03190865e
Felipe Junges <felipejunges@yahoo.com.br>
2026-02-11 10:28:39 -0300

    wip

--- FileKeeper.Core/Interfaces/Abstraction/IFileSystem.cs
+++ FileKeeper.Core/Interfaces/Abstraction/IFileSystem.cs
@@ -7,19 +7,19 @@ public interface IFileSystem
     string[] GetFiles(string path, string searchPattern, SearchOption searchOption);
     string[] GetDirectories(string path);
     void DeleteDirectory(string path, bool recursive);
-        
+
     bool FileExists(string path);
     void CopyFile(string source, string dest);
     void MoveFile(string source, string dest);
     string ReadAllText(string path);
     void WriteAllText(string path, string contents);
-        
+
     long GetFileLength(string path);
     DateTime GetFileLastWriteTimeUtc(string path);
-        
+
     DateTime GetDirectoryCreationTimeUtc(string path);
     string GetDirectoryName(string path);
-        
+
     void DeleteFile(string path);
     DateTime GetFileCreationTimeUtc(string path);
 }
