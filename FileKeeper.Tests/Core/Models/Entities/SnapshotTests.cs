6f5b376cba576e6305238ee1e3a07e7f1759bbfb
Felipe Junges <felipejunges@yahoo.com.br>
2026-03-29 12:09:11 -0300

    ✅ Added a Little UnitTest for Snapshot entity

---
+++ FileKeeper.Tests/Core/Models/Entities/SnapshotTests.cs
@@ -0,0 +1,17 @@
+using FileKeeper.Core.Models.Entities;
+
+namespace FileKeeper.Tests.Core.Models.Entities;
+
+public class SnapshotTests
+{
+    [Fact]
+    public void Snapshot_SnapshotName_ShouldReflectTheId()
+    {
+        var id = Guid.Parse("019d3a22-9a87-75ae-ac95-7222c74df7c4");
+        var expectedName = "019d3a22";
+        var snapshot = new Snapshot(id, DateTime.UtcNow, []);
+        
+        Assert.Equal(id, snapshot.Id);
+        Assert.Equal(expectedName, snapshot.SnapshotName);
+    }
+}
